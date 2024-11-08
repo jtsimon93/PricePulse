b3e7f7950aa4e4212a1b75360587b9656c4f51a2
Justin Simon <jtsimon@jtsimon.dev>
2024-11-08 13:57:21 -0500

    Initial commit

---
+++ PricePulse.Core/Entities/ConsumerPriceIndexSeries.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PricePulse.Core.Entities;
+
+public class ConsumerPriceIndexSeries
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [MaxLength(255)]
+    [Required]
+    public string SeriesId { get; set; }
+    
+    [MaxLength(255)]
+    [Required]
+    public string SeriesTitle { get; set; }
+    
+    [MaxLength(255)]
+    [Required]
+    public string Area { get; set; }
+    
+    [MaxLength(255)]
+    [Required]
+    public string Item { get; set; }
+    
+    [Required]
+    public int Year { get; set; }
+    
+    [Required]
+    public int Month { get; set; }
+    
+    [Required]
+    public decimal Value { get; set; }
+    
+    [Required]
+    public bool IsSeasonallyAdjusted { get; set; }
+    
+    [Required]
+    public string Frequency { get; set; }
+    
+    [Required]
+    public string Source { get; set; }
+    
+    [Required]
+    public string UnitOfMeasure { get; set; }
+    
+    [Required]
+    public string DataType { get; set; }
+    
+    [Required]
+    public string Currency { get; set; } = "USD";
+    
+    [Required]
+    public DateTime DateRetrieved { get; set; }
+    
+    [Required]
+    public string? Description { get; set; }
+    
+    [Required]
+    public bool IsEnergyItem { get; set; }
+    
+    [Required]
+    public bool IsFoodItem { get; set; }
+    
+    [Required]
+    public bool IsHousingItem { get; set; }
+    
+    [Required]
+    public bool IsMedicalItem { get; set; }
+    
+    [Required]
+    public bool IsTransportationItem { get; set; }
+    
+    [Required]
+    public bool IsApparelItem { get; set; }
+}
