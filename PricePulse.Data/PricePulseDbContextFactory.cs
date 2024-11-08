ccce59f38479f103e00f38ec88b0befccde08806
Justin Simon <jtsimon@jtsimon.dev>
2024-11-08 16:01:24 -0500

    Add design time context

---
+++ PricePulse.Data/PricePulseDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace PricePulse.Data
+{
+    public class PricePulseDbContextFactory : IDesignTimeDbContextFactory<PricePulseDbContext>
+    {
+        public PricePulseDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PricePulseDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new PricePulseDbContext(optionsBuilder.Options);
+        }
+    }
+}
