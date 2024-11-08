9d61882fbc80f22d15729ab5da793c2422de93d9
Justin Simon <jtsimon@jtsimon.dev>
2024-11-08 15:14:19 -0500

    Add series repository

---
+++ PricePulse.Core/Interfaces/IConsumerPriceIndexSeriesRepository.cs
@@ -0,0 +1,10 @@
+﻿using PricePulse.Core.Entities;
+
+namespace PricePulse.Core.Interfaces;
+
+public interface IConsumerPriceIndexSeriesRepository
+{
+    Task<ConsumerPriceIndexSeries> AddAsync(ConsumerPriceIndexSeries series);
+    Task<ConsumerPriceIndexSeries?> GetBySeriesIdAsync(string seriesId);
+    Task<IEnumerable<ConsumerPriceIndexSeries>> GetAllAsync();
+}
---
+++ PricePulse.Data/Repositories/ConsumerPriceIndexSeriesRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PricePulse.Core.Entities;
+using PricePulse.Core.Interfaces;
+
+namespace PricePulse.Data.Repositories;
+
+public class ConsumerPriceIndexSeriesRepository : IConsumerPriceIndexSeriesRepository
+{
+    private readonly PricePulseDbContext _dbContext;
+    private readonly ILogger<ConsumerPriceIndexSeriesRepository> _logger;
+    
+    public ConsumerPriceIndexSeriesRepository(PricePulseDbContext dbContext, ILogger<ConsumerPriceIndexSeriesRepository> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+    
+    public async Task<ConsumerPriceIndexSeries> AddAsync(ConsumerPriceIndexSeries series)
+    {
+        _dbContext.ConsumerPriceIndexSeries.Add(series);
+        await _dbContext.SaveChangesAsync();
+        return series;
+    }
+    
+    public async Task<ConsumerPriceIndexSeries?> GetBySeriesIdAsync(string seriesId)
+    {
+        return await _dbContext.ConsumerPriceIndexSeries
+            .FirstOrDefaultAsync(series => series.SeriesId == seriesId);
+    }
+    
+    public async Task<IEnumerable<ConsumerPriceIndexSeries>> GetAllAsync()
+    {
+        return await _dbContext.ConsumerPriceIndexSeries
+            .AsNoTracking()
+            .ToListAsync();
+    }
+    
+    
+    
+}
