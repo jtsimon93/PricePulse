4112b2c3eb60d0b13bd2ae93cb6d2dce72b54605
Justin Simon <jtsimon@jtsimon.dev>
2024-11-08 14:21:15 -0500

    Create data project

---
+++ PricePulse.Data/PricePulseDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PricePulse.Core.Entities;
+
+namespace PricePulse.Data;
+
+public class PricePulseDbContext : DbContext
+{
+    public PricePulseDbContext(DbContextOptions<PricePulseDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<ConsumerPriceIndexSeries> ConsumerPriceIndexSeries { get; set; }
+    public DbSet<ConsumerPriceIndexEntry> ConsumerPriceIndexEntries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ConsumerPriceIndexSeries>()
+            .HasIndex(c => new { c.SeriesId })
+            .IsUnique();
+
+        // Composite unique constraint on Year, Month, and SeriesId to avoid duplicates
+        modelBuilder.Entity<ConsumerPriceIndexEntry>()
+            .HasIndex(e => new { e.ConsumerPriceIndexSeriesId, e.Year, e.Month })
+            .IsUnique();
+    }
+    
+}
